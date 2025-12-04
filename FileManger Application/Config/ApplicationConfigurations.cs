c9f1c35c806b2f9de00f206f0bf5e917228e4f8e
TUNDE DANIEL <omo.hope.blog@gmail.com>
2025-12-04 12:44:35 +0100

    First Comit

---
+++ FileManger Application/Config/ApplicationConfigurations.cs
@@ -0,0 +1,29 @@
+﻿using FileManger_Application.Data;
+using FileManger_Application.Model;
+using FileManger_Application.Repositories;
+using FileManger_Application.UnitOfWorks;
+using Microsoft.AspNetCore.Identity;
+
+namespace FileManger_Application.Config
+{
+    public static class ApplicationConfigurations
+    {
+        public static IServiceCollection ApplicationConfiguration(this IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            serviceCollection.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+            {
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 4;
+                options.Password.RequiredUniqueChars = 1;
+            }).AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            serviceCollection.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>();
+            return serviceCollection;
+        }
+    }
+}
--- FileManger Application/Program.cs
+++ FileManger Application/Program.cs
@@ -5,22 +5,10 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
-builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(p =>
-{
-    p.Password.RequiredLength = 5;
-    p.Password.RequireNonAlphanumeric = false;
-    p.Password.RequireDigit = false;
-    p.Password.RequireLowercase = false;
-    p.Password.RequireUppercase = false;
-    p.Password.RequiredUniqueChars = 1;
 
-}
-)
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddDefaultTokenProviders();
 
 var app = builder.Build();
 
-
+app.Services.Applicatio
 
 app.Run();
